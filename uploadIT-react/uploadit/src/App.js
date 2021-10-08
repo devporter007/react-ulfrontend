bd5ea8b3341344537e7ade91189b4c1d0881bcac
devporter007 <portzero7@protonmail.com>
2021-10-08 17:49:19 +0530

    Init

---
+++ uploadIT-react/uploadit/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import './custom.css';
+function App() {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+            <form method="post" action="#" id="#">
+                  <div class="form-group files">
+                    <label>&nbsp;&nbsp;Upload Your File</label>
+                    <input name="inp" type="file" class="form-control" multiple="" /> 
+                  </div>
+                  
+                
+              </form>   
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
